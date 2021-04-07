f9a9e9f6439044f4fe76f650cdcfabfcefcb56fc
Ethan Jackson <ethantjackson44@gmail.com>
2021-04-07 12:47:55 -0700

    add contact item

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,22 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 const ContactItem = ({ contact }) => {
   const { name, id, email, phone, type } = contact;
   return (
-    <div className='card bg-light'>
-      <h3 className='text-primary text-left'>
-        {name}{' '}
+    <div className="card bg-light">
+      <h3 className="text-primary text-left">
+        {name}{" "}
         <span
-          style={{ float: 'right' }}
+          style={{ float: "right" }}
           className={
-            'badge ' +
-            (type === 'professional' ? 'badge-success' : 'badge-primary')
+            "badge " +
+            (type === "professional" ? "badge-success" : "badge-primary")
           }
         >
           {type.charAt(0).toUpperCase() + type.slice(1)}
         </span>
       </h3>
+      {email && (
+        <li>
+          <i class="fas fa-envelope-open"></i> {email}
+        </li>
+      )}
+      {phone && (
+        <li>
+          <i class="fas fa-phone"></i> {phone}
+        </li>
+      )}
+      <button class="btn btn-dark btn-sm">Edit</button>
+      <button class="btn btn-danger btn-sm">Delete</button>
     </div>
   );
 };
